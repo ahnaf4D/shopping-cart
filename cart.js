335d6e272f48e0ac75259a07dd86962cb7d3dd02
ahnaf4D <ahnaftahmid802@gmail.com>
2024-02-16 16:27:17 +0600

    problem solved

--- cart.js
+++ cart.js
@@ -1,9 +1,8 @@
 const increaseDecreaseOne = document.getElementById('increase-decrase-1');
 const itemIncrease = document.getElementById('item-increase');
-let count = 0;
 itemIncrease.addEventListener('click', function (event) {
-    count++;
     let increaseDecreaseOneValue = increaseDecreaseOne.value;
     let increaseDecreaseNums = parseInt(increaseDecreaseOneValue);
-    increaseDecreaseOneValue = count;
+    increaseDecreaseOne.value = increaseDecreaseNums + 1;
+
 })
