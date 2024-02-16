d5588e35cf02bd930742b7ccd34a5489ba39344b
ahnaf4D <ahnaftahmid802@gmail.com>
2024-02-16 16:45:53 +0600

    decrease and increase functionality added to the website

--- cart.js
+++ cart.js
@@ -1,8 +1,15 @@
 const increaseDecreaseOne = document.getElementById('increase-decrase-1');
 const itemIncrease = document.getElementById('item-increase');
+const itemDecrease = document.getElementById('item-decrease');
 itemIncrease.addEventListener('click', function (event) {
     let increaseDecreaseOneValue = increaseDecreaseOne.value;
-    let increaseDecreaseNums = parseInt(increaseDecreaseOneValue);
-    increaseDecreaseOne.value = increaseDecreaseNums + 1;
-
+    let increaseDecreaseOneValueNum = parseInt(increaseDecreaseOneValue);
+    increaseDecreaseOneValueNum += 1;
+    increaseDecreaseOne.value = increaseDecreaseOneValueNum;
+})
+itemDecrease.addEventListener('click', function () {
+    let increaseDecreaseOneValue = increaseDecreaseOne.value;
+    let increaseDecreaseOneValueNum = parseInt(increaseDecreaseOneValue);
+    increaseDecreaseOneValueNum -= 1;
+    increaseDecreaseOne.value = increaseDecreaseOneValueNum;
 })
